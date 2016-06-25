4da0e69a66c88b14404bcfff94346a6e88c9ffe1
Brian W Bush <b.w.bush@acm.org>
2016-06-24 20:01:35 -0600

    added orphan instances

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,13 @@
 # Revision history for cesds-records
 
-## 0.1.0.0  -- YYYY-mm-dd
+## 0.1.2.0  -- 2016-06-24
+
+* Added orphan Read, FromJSON, and ToJSON instances for URI
+
+## 0.1.1.0  -- 2016-06-23
+
+* Split types into separate files.
+
+## 0.1.0.0  -- 2016-06-23
 
 * Translation of API specification into data types.
--- src/CESDS/Types/Model.hs
+++ src/CESDS/Types/Model.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric  #-}
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 
 module CESDS.Types.Model (
   ModelIdentifier
@@ -8,12 +10,13 @@ module CESDS.Types.Model (
 ) where
 
 
-import CESDS.Types (Color, Generation, Identifier, Tags)
-import CESDS.Types.Variable (Domain, VariableIdentifier)
-import Data.Aeson (FromJSON, ToJSON)
-import Data.Text (Text)
+import CESDS.Types (Generation, Identifier, Tags)
+import CESDS.Types.Variable (VariableIdentifier)
+import Data.Aeson (Value(String))
+import Data.Aeson.Types (FromJSON(parseJSON), ToJSON(toJSON), typeMismatch)
+import Data.Text (Text, pack, unpack)
 import GHC.Generics (Generic)
-import Network.URI (URI)
+import Network.URI (URI, parseURI)
 
 
 type ModelIdentifier = Identifier
@@ -33,3 +36,16 @@ data Model =
   , timeKey          :: Maybe VariableIdentifier
   }
     deriving (Eq, FromJSON, Generic, Read, Show, ToJSON)
+
+
+instance Read URI where
+  readsPrec _ s = case parseURI s of
+                    Nothing  -> []
+                    Just uri -> [(uri, "")]
+
+instance FromJSON URI where
+  parseJSON invalid@(String s) = maybe (typeMismatch "URI" invalid) return . parseURI $ unpack s
+  parseJSON invalid            = typeMismatch "URI" invalid
+
+instance ToJSON URI where
+  toJSON = String . pack . show
