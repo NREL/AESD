e1147f569291a990a4babe05fd97c9a55353243a
Brian W Bush <b.w.bush@acm.org>
2016-06-26 04:59:39 -0600

    work in progress on testing models

--- src/TestJSON.hs
+++ src/TestJSON.hs
@@ -8,16 +8,18 @@ module Main (
 ) where
 
 
-import CESDS.Types (Color)
+import CESDS.Types (Color, Tags(..))
+import CESDS.Types.Model (Model(..))
 import Control.Arrow ((&&&))
 import Control.Monad (unless)
-import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import Data.Aeson (FromJSON, ToJSON, Value(..), decode, encode)
 import Data.Colour.SRGB (sRGB24)
 import Data.Maybe (fromJust)
+import Data.Text (Text, pack)
 import Network.URI (URI, parseURI)
 import System.Exit (exitFailure)
 import Test.QuickCheck.Arbitrary (Arbitrary(..))
-import Test.QuickCheck.Gen (Gen, choose, listOf1, sample)
+import Test.QuickCheck.Gen (Gen, choose, elements, listOf, listOf1, sample)
 import Test.QuickCheck.Property (Property, property)
 import Test.QuickCheck.Test (isSuccess, quickCheckResult)
 
@@ -42,12 +44,12 @@ prop_color_json :: Color -> Property
 prop_color_json = checkEncodeDecode
 
 
-characters :: Gen String
-characters = listOf1 $ choose ('a', 'z')
-
-
 instance Arbitrary URI where
-  arbitrary = fromJust . parseURI . ("http://" ++) <$> characters -- FIXME: Make this more rigorous, even though it is good enough for testing this package.
+  arbitrary = -- FIXME: Make this more rigorous, even though it is good enough for testing this package.
+    fromJust
+      . parseURI
+      . ("http://" ++)
+      <$> listOf1 (choose ('a', 'z'))
 
 
 prop_uri_io :: URI -> Property
@@ -58,11 +60,28 @@ prop_uri_json :: URI -> Property
 prop_uri_json = checkEncodeDecode
 
 
+instance Arbitrary Text where
+  arbitrary = pack <$> listOf1 (elements $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'])
+
+
+instance Arbitrary Tags where
+  arbitrary = Tags <$> listOf ((,) <$> arbitrary <*> (String <$> arbitrary)) -- FIXME: Make this more rigorous.
+      
+
+
+instance Arbitrary Model where
+  arbitrary = Model <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+
+prop_model_json :: Model -> Property
+prop_model_json = checkEncodeDecode
+
+
 main :: IO ()
 main =
   do
---  sample (arbitrary :: Gen Color)
---  sample (arbitrary :: Gen URI)
+    sample $ encode <$> (arbitrary :: Gen Model)
+    sample (arbitrary :: Gen Model)
     results <-
       sequence
         [
@@ -70,5 +89,6 @@ main =
         , quickCheckResult prop_color_json
         , quickCheckResult prop_uri_io
         , quickCheckResult prop_uri_json
+        , quickCheckResult prop_model_json
         ]
     unless (all isSuccess results) exitFailure
