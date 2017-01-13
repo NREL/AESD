ed082f36cfc7ac2cb414da79a915b2d46494e365
Brian W Bush <b.w.bush@acm.org>
2017-01-13 13:07:17 -0700

    implemented requesting record data

--- src/cesds-records.js
+++ src/cesds-records.js
@@ -1,5 +1,8 @@
+var debug = true;
+
 
 var handlers = null;
+
 
 function connect(wsURL) {
   var result = new WebSocket(wsURL);
@@ -10,6 +13,11 @@ function connect(wsURL) {
       var buffer = new Uint8Array(event.data);
       var response = proto.cesds.Response.deserializeBinary(buffer);
       var id = response.getId();
+      if (debug) {
+        console.log("Response ID: " , id.getValue());
+        console.log("  Chunk:     " , response.getChunkId());
+        console.log("  Next chunk: ", response.getNextChunkId());
+      }
       if (id in handlers) {
         var answer = handlers[id](response);
         if (answer.done)
@@ -24,24 +32,30 @@ function connect(wsURL) {
   return result;
 }
 
+
 function disconnect(connection) {
   connection.close();
 }
 
+
 function addHandler(id, f) {
   handlers[id] = f;
 }
+
 
 var ws = connect("ws://127.0.0.1:50374");
 
 
 var version = 3;
 
+
 var currentId = 0;
+
 
 function nextId() {
   return optionalUInt32(++currentId);
 }
+
 
 function optionalInt32(x) {
   var result = new proto.cesds.OptionalInt32()
@@ -49,17 +63,20 @@ function optionalInt32(x) {
   return result;
 }
 
+
 function optionalUInt32(x) {
   var result = new proto.cesds.OptionalUInt32()
   result.setValue(x);
   return result;
 }
 
+
 function optionalString(x) {
   var result = new proto.cesds.OptionalString()
   result.setValue(x);
   return result;
 }
+
 
 function toVariable(v) {
   return {
@@ -72,6 +89,7 @@ function toVariable(v) {
          };
 }
 
+
 function toModel(m) {
   return {
            id        : m.getModelId()
@@ -80,6 +98,7 @@ function toModel(m) {
          , variables : m.getVariablesList().map(toVariable)
          };
 }
+
 
 function onResponse(response, handleError, handleModels, handleData, handleBookmarks) {
   if (handleError != null && response.hasError())
@@ -95,6 +114,7 @@ function onResponse(response, handleError, handleModels, handleData, handleBookm
     return {done : false, notify : handleError, value : null};
   }
 }
+
 
 function requestModelsMetadata(connection, modelId, notify, notifyError) {
   var request = new proto.cesds.Request();
@@ -118,6 +138,102 @@ function requestModelsMetadata(connection, modelId, notify, notifyError) {
       return {done : result.complete, notify : notify, value : result};
     };
   addHandler(request.getId(), response => onResponse(response, notifyError != null ? handleError : null, handleModels, null, null));
+  connection.send(request.serializeBinary());
+  return result;
+}
+
+
+function fromValue(v) {
+  if (v.hasRealValue())
+    return v.getRealValue();
+  else if (v.hasIntegerValue())
+    return v.getIntegerValue();
+  else if (v.hasStringValue())
+    return v.getStringValue();
+  else
+    return null;
+}
+
+
+function fromRecord(r) {
+  var values = new Object();
+  r.getVariablesList().forEach(vv => values[vv.getVarId()] = fromValue(vv.getValue()));
+  return {
+           id     : r.getRecordId()
+         , values : values
+         };
+}
+
+
+function fromRecordList(rl) {
+  return rl.getRecordsList().map(fromRecord);
+}
+
+
+function fromRecordTable(rt) {
+  var vids = rt.getVarIdsList();
+  var nv = vids.length;
+  var rids = rt.getRecIdsList();
+  var nr = rids.length;
+  var vals = null;
+  if (rt.hasReals())
+    vals = rt.getReals().getValuesList();
+  else if (rt.hasIntegers())
+    vals = rt.getIntegers().getValuesList();
+  else if (rt.hasStrings())
+    vals = rt.getStrings().getValuesList();
+  else
+    vals = Array(nv * nr);
+  var rows = [];
+  for (var i = 0; i < nr; ++i) {
+    var obj = new Object();
+    for (var j = 0; j < nv; ++j)
+      obj[vids[j]] = vals[i * nr + j];
+    rows.push({id : rids[i], values : obj});
+  }
+  return rows;
+}
+
+
+function fromRecordData(rd) {
+  if (rd.hasList())
+    return fromRecordList(rd.getList());
+  else if (rd.hasTable())
+    return fromRecordTable(rd.getTable());
+  else
+    console.error("Invalid record data: ", rd);
+}
+
+
+function requestRecordsData(connection, modelId, maxRecords, variableIds, bookmarkId, notify, notifyError) {
+  var request = new proto.cesds.Request();
+  request.setVersion(version);
+  request.setId(nextId());
+  var d = new proto.cesds.RequestRecordsData();
+  d.setModelId(modelId);
+  if (maxRecords != null)
+    d.setMaxRecords(maxRecords);
+  if (variableIds != null)
+    d.setVariableIdsList(variableIds);
+  if (bookmarkId != null)
+    d.setBookmarkId(bookmarkId);
+  request.setRecordsData(d);
+  var result = {complete : false, data : []};
+  var handleError =
+    function(e) {
+      console.warn("Error response: ", e);
+      result.complete = false;
+      return {done : true, notify : notifyError, value : e};
+    };
+  var handleData =
+    function(c, nc, rd) {
+      result.data.push.apply(result.data, fromRecordData(rd));
+      result.complete = nc < 1;
+      return {done : result.complete, notify : notify, value : result};
+    };
+  if (debug)
+    console.log("Request ID: ", request.getId().getValue());
+  addHandler(request.getId(), response => onResponse(response, notifyError != null ? handleError : null, null, handleData, null));
   connection.send(request.serializeBinary());
   return result;
 }
